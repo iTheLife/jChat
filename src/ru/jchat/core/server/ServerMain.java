60c4114db82bdd64a7806b8cb024738053a79626
thelifekuz@gmail.com <sTd389534r>
2018-12-11 00:21:46 +0300

    Initial commit

---
+++ src/ru/jchat/core/server/ServerMain.java
@@ -0,0 +1,11 @@
+package ru.jchat.core.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
